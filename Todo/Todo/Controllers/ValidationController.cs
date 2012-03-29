63ca2309f7e217a52085f6b430f4a02716e1503d
Kevin Jones <kevinj@mantiso.com>
2012-03-29 11:44:30 +0100

    Validation

---
+++ Todo/Todo/Controllers/ValidationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Todo.Controllers
+{
+    public class ValidationController : Controller
+    {
+        public JsonResult ValidateExtension(string file, string title, string entry)
+        {
+            bool result = false;
+            if(file !=  null)
+            {
+                string ext = file.Split('.').Last();
+                if (ext == "png")
+                {
+                    result = true;
+                }
+            }
+            return Json(result);
+        }
+    }
+}
--- Todo/Todo/Models/NewTodoVM.cs
+++ Todo/Todo/Models/NewTodoVM.cs
@@ -1,8 +1,30 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Mvc;
+
 namespace Todo.Models
 {
-    public class NewTodoVM
+    public class NewTodoVM : IValidatableObject
     {
+        [Required]
         public string Title { get; set; }
         public string Entry { get; set; }
+        [Remote("ValidateExtension", "Validation", HttpMethod = "POST", ErrorMessage = "Invalid Extension", AdditionalFields = "Title, Entry")]
+        public string File { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (File != null)
+            {
+                string ext = File.Split('.').Last();
+                if (ext != "png")
+                {
+                    yield return new ValidationResult("Invalid extension", new[]{"File"});
+                }
+            }
+
+        }
     }
 }
